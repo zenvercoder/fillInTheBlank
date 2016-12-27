7ca3ad2aa09a6275ecf8fd74d66bbe05cc642cf3
Lisa Hull <lisa@jchull.com>
2016-12-26 17:13:44 -0700

    links added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+
 import Footer from './components/Footer.js';
 import Button from './components/Button.js';
 
